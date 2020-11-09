5f72cc987b1f8603e3edc0c34c85d6eab06f3c9f
MrCrashocaster <mrcrashocaster@gmail.com>
2020-11-08 20:57:26 -0600

    Updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyB9JBrbwZ-7ykJn5dLoIoZrWonWmfhLA78",
-    authDomain: "delta-buds.firebaseapp.com",
-    databaseURL: "https://delta-buds.firebaseio.com",
-    projectId: "delta-buds",
-    storageBucket: "delta-buds.appspot.com",
-    messagingSenderId: "286539392906",
-    appId: "1:286539392906:web:dbdddbca9b7db99d268a73",
-    measurementId: "G-VHT604Q9NY"
+    apiKey: "AIzaSyBz4tszMolSOkYM3fyKzEWUclGrGttHuEY",
+    authDomain: "kevin-harrell.firebaseapp.com",
+    databaseURL: "https://kevin-harrell.firebaseio.com",
+    projectId: "kevin-harrell",
+    storageBucket: "kevin-harrell.appspot.com",
+    messagingSenderId: "114965867678",
+    appId: "1:114965867678:web:457e5451066afbaa445f8f",
+    measurementId: "G-6HWLC5DJ6B"
   }
 };
 
